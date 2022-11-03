8c9e43454fb4c3d12421c7922f192f15083b0b7d
Renuka Choudhary <renurunrun@gmail.com>
2022-11-03 19:51:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>About Us</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
+       type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
+       electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+       sheets containing Lorem Ipsum passages,and more recently with desktop publishing software like Aldus PageMaker including 
+       versions of Lorem Ipsum.
+       </p>
+       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
+       type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
+       electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+       sheets containing Lorem Ipsum passages,and more recently with desktop publishing software like Aldus PageMaker including 
+       versions of Lorem Ipsum.
+       </p>
+       <p>Created with ❤️.Happy Coding...</p>
+       <p>Renuka Choudhary</p>
     </div>
   );
 }
